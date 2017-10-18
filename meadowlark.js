ae67fcd343dc1716d7be98e0cb3a13694a27e557
kuldmitri <KulDmitriBy@gmail.com>
2017-10-18 12:13:39 +0300

    Fortunes вынесены в отдельный модуль

--- meadowlark.js
+++ meadowlark.js
@@ -1,3 +1,4 @@
+var fortune = require('./lib/fortune.js');
 var express = require('express');
 
 var app = express();
@@ -12,31 +13,21 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-var fortuneCookies = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
-
 app.get('/', function (req, res) {
     res.render('home');
 });
 app.get('/about', function (req, res) {
-    var randomFortune =
-        fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
-    res.render('about', {fortune: randomFortune});
+    res.render('about', {fortune: fortune.getFortune()});
 });
 
 // 404 catch-all handler (middleware)
-app.use(function (req, res, next) {
+app.use(function (req, res) {
     res.status(404);
     res.render('404');
 });
 
 // 500 error handler (middleware)
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
     console.error(err.stack);
     res.status(500);
     res.render('500');
